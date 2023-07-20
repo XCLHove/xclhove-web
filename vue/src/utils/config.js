b535bd33b4e493a5f771bdfdcc95456e4a65fe9f
XCLHove <2959169303@qq.com>
2023-07-20 14:08:26 +0800

    修改读取配置的方式

--- vue/src/utils/config.js
+++ vue/src/utils/config.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 
-let config = {
-    serverUrl: 'http://localhost:8081',
-    beiAnHao: '蜀ICP备2023003714号',
-}
 //读取配置
-axios.get('/config.json').then(result => {
-    config = result.data ? result.data : config
-})
+async function readConfig() {
+    let config = {}
+    await axios.get('/config.json').then(result => {
+        config = result.data ? result.data : config
+    })
+    return config
+}
+
+const config = await readConfig()
 //导出以便引用
 export default config
