6bbf20aee6040ade65e0bc411decac07cbf43e69
XCLHove <2959169303@qq.com>
2023-07-26 16:17:04 +0800

    添加Head组件

---
+++ vue/src/components/Head.vue
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<template>
+  <div class="head"><slot></slot></div>
+</template>
+
+<style scoped lang="less">
+.head {
+  width: 100%;
+  font-size: 50px;
+  text-align: center;
+  padding: 10px 0;
+}
+</style>
