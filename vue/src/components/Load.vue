ae0653febe5824aa0e490f39d6c8c27cf552adf3
XCLHove <2959169303@qq.com>
2023-07-27 23:57:48 +0800

    添加页面加载动画组件

---
+++ vue/src/components/Load.vue
@@ -0,0 +1,31 @@
+<script setup>
+import {ref} from "vue";
+import sleep from "@/utils/sleep";
+
+const pageLoad = ref(true)
+const pageShow = ref(false)
+const pageInit = async () => {
+  await sleep(0.5).then(() => {
+    pageLoad.value = false
+  })
+  pageShow.value = !pageShow.value
+}
+pageInit()
+</script>
+
+<template>
+  <div class="loadContainer" v-loading="pageLoad">
+    <Transition>
+      <div class="showContainer" v-show="pageShow">
+        <slot></slot>
+      </div>
+    </Transition>
+  </div>
+</template>
+
+<style scoped lang="less">
+.loadContainer {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
