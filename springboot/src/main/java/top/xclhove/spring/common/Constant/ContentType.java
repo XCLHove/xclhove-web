3ee134c5eeec45386bd908212513a9b9ef8ff25b
XCLHove <2959169303@qq.com>
2023-07-30 02:03:06 +0800

    添加常量

---
+++ springboot/src/main/java/top/xclhove/spring/common/Constant/ContentType.java
@@ -0,0 +1,5 @@
+package top.xclhove.spring.common.Constant;
+
+public interface ContentType {
+    String X_WWW_FORM_URLENCODED = "application/x-www-form-urlencoded";
+}
---
+++ springboot/src/main/java/top/xclhove/spring/common/Constant/OneDriveKey.java
@@ -0,0 +1,11 @@
+package top.xclhove.spring.common.Constant;
+
+public interface OneDriveKey {
+    String API_URL = "https://login.microsoftonline.com/common/oauth2/v2.0/token";
+    String CLIENT_ID = "client_id";
+    String REDIRECT_URI = "redirect_uri";
+    String CLIENT_SECRET = "client_secret";
+    String REFRESH_TOKEN = "refresh_token";
+    String GRANT_TYPE = "grant_type";
+    String ACCESS_TOKEN = "access_token";
+}
