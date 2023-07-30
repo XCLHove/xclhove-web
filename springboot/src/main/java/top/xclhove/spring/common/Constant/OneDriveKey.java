a5ef3a17e12fdff90f204a3a99e5e90faab13b5a
XCLHove <2959169303@qq.com>
2023-07-30 23:48:46 +0800

    修改OneDriveKey结构

--- springboot/src/main/java/top/xclhove/spring/common/Constant/OneDriveKey.java
+++ springboot/src/main/java/top/xclhove/spring/common/Constant/OneDriveKey.java
@@ -1,11 +1,18 @@
 package top.xclhove.spring.common.Constant;
 
 public interface OneDriveKey {
-    String API_URL = "https://login.microsoftonline.com/common/oauth2/v2.0/token";
     String CLIENT_ID = "client_id";
     String REDIRECT_URI = "redirect_uri";
     String CLIENT_SECRET = "client_secret";
     String REFRESH_TOKEN = "refresh_token";
     String GRANT_TYPE = "grant_type";
     String ACCESS_TOKEN = "access_token";
+    String BEARER = "Bearer";
+    public interface Api {
+        String TOKEN = "https://login.microsoftonline.com/common/oauth2/v2.0/token";
+        String FILE = "https://graph.microsoft.com/v1.0";
+    }
+    public interface Path {
+        String root = "/me/drive/root/children";
+    }
 }
