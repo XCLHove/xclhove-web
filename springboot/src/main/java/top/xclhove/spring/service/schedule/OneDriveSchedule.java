c3a96f96dfadbd34ff4f3fc058e9e544c53c99fc
XCLHove <2959169303@qq.com>
2023-08-01 21:29:23 +0800

    修改刷新 OneDrive access_token 的定时任务

--- springboot/src/main/java/top/xclhove/spring/service/schedule/OneDriveSchedule.java
+++ springboot/src/main/java/top/xclhove/spring/service/schedule/OneDriveSchedule.java
@@ -1,5 +1,6 @@
 package top.xclhove.spring.service.schedule;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -8,15 +9,17 @@ import top.xclhove.spring.service.impl.OneDriveServiceImpl;
 
 @Configuration
 @EnableScheduling
+@Slf4j
 public class OneDriveSchedule {
     @Autowired
     private OneDriveServiceImpl oneDriveService;
 
     /**
-     * 项目启动 10 秒后, 每 5 分钟执行一次刷新 OneDrive access_token 的定时任务.
+     * 项目启动 10 分钟后, 每 5 分钟执行一次刷新 OneDrive access_token 的定时任务.
      */
-    @Scheduled(fixedRate = 1000 * 60 * 5, initialDelay = 1000 * 10)
+    @Scheduled(fixedRate = 1000 * 60 * 5, initialDelay = 1000 * 60 * 10)
     private void refreshOneDriveAccessToken() {
-        oneDriveService.refreshOneDriveAccessToken();
+        log.info("执行刷新 OneDrive access_token 的定时任务");
+        oneDriveService.refreshAllOneDriveAccessToken();
     }
 }
